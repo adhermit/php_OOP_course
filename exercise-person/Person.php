48d37d616e88253f2db4901391c291099fb78a18
adhermit <maharjanraman456@gmail.com>
2025-01-08 15:54:15 +0100

    new

---
+++ exercise-person/Person.php
@@ -0,0 +1,17 @@
+<?php
+class Person{
+    public string $firstName;
+    public string $lastName;
+    public int $age;
+
+    public function __construct(string $firstName, string $lastName, int $age){
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->age = $age;
+     }
+
+     public function getFullName(){
+        return $this->firstName . ' ' . $this->lastName;
+     }
+
+}

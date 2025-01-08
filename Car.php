f1ce3d8a0d7670887f3bc5bcb90464c12eac59c5
adhermit <maharjanraman456@gmail.com>
2025-01-08 13:55:27 +0100

    new

--- Car.php
+++ Car.php
@@ -1,12 +1,12 @@
 <?php
-Class Car
+class Car
 {
     public string $brand;
     public float $maxSpeed;
 }
 
 $car = new Car();
-$car-> brand = 'Toyota';
-$car-> maxSpeed = 225;
+$car->brand = 'Toyota';
+$car->maxSpeed = 225;
 
 var_dump($car);
